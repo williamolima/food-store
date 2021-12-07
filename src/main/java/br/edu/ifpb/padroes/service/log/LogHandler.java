c5312b7021268eec4f151b3f789c683d9eede645
William Oliveira Lima <williamolima@icloud.com>
2021-12-07 13:25:33 -0300

    Ajuste padrao strategy
    adicionado codigo com ajustes

--- src/main/java/br/edu/ifpb/padroes/service/log/LogHandler.java
+++ src/main/java/br/edu/ifpb/padroes/service/log/LogHandler.java
@@ -2,20 +2,29 @@ package br.edu.ifpb.padroes.service.log;
 
 public class LogHandler {
 
-    public LogHandler(LogHandlerType type) {
-        this.type = type;
-    }
-
     public enum LogHandlerType { DATABASE, FILE}
 
-    private LogHandlerType type;
-
-    public void log(String message) {
-        if (type.equals(LogHandlerType.DATABASE)) {
-            System.out.println("save data to database");
-        } else if (type.equals(LogHandlerType.FILE)) {
-            System.out.println("save data to a file");
-        }
+    public interface LogGerador{
+        LogHandler gerarLog(LogHandlerType.DATABASE, LogHandlerType.FILE);
     }
+
+    public LogGerador logGerador;
+
+    public LogHandler gerarLog(LogHandlerType.DATABASE, LogHandlerType.FILE) {
+        return logGerador.gerarLog(DATABASE, FILE);
+    }
+    
+
+    // public LogHandler(LogHandlerType type) {
+    //     this.type = type;
+    // }
+    // private LogHandlerType type;
+    // public void log(String message) {
+    //     if (type.equals(LogHandlerType.DATABASE)) {
+    //         System.out.println("save data to database");
+    //     } else if (type.equals(LogHandlerType.FILE)) {
+    //         System.out.println("save data to a file");
+    //     }
+    // }
 
 }

f958c9d4ae9afa64bf298a15e394f4c7e7e63cf9
William Oliveira Lima <williamolima@icloud.com>
2021-12-08 18:09:16 -0300

    Resolução segundo problema
    Aplicando padrão State no serviço Payment

--- src/main/java/br/edu/ifpb/padroes/service/payment/PaymentService.java
+++ src/main/java/br/edu/ifpb/padroes/service/payment/PaymentService.java
@@ -6,26 +6,59 @@ public class PaymentService {
         CREDIT_CARD, DEBIT, BILLET, PAYPAL
     }
 
-    public void doPayment(PaymentType type) throws Exception {
-        switch (type) {
-            case CREDIT_CARD:
-                System.out.println("Do credit card payment!");
-                break;
-            case DEBIT:
-                System.out.println("Do debit payment!");
-                break;
-            case BILLET:
-                System.out.println("Do billet payment!");
-                break;
-            case PAYPAL:
-                System.out.println("Do paypal payment!");
-                break;
-            default:
-                throw new Exception("unknown payment method");
-        }
-
-
-
+    public interface PaymentState {
+        PaymentState pay_credit();
+        PaymentState pay_debit();
+        PaymentState pay_billet();
+        PaymentState pay_paypal();
     }
+
+    public class Payment{
+        private PaymentState estadoAtual;
+
+        public Payment(){
+            this.estadoAtual = new PaymentType;
+        }
+    }
+
+    public Payment(PaymentState estadoAtual){
+        this.estadoAtual = estadoAtual;
+    }
+
+    public void pay_credit(){
+        this.estadoAtual = estadoAtual.CREDIT_CARD;
+    }
+
+    public void pay_debit(){
+        this.estadoAtual = estadoAtual.DEBIT;
+    }
+
+    public void pay_billet(){
+        this.estadoAtual = estadoAtual.BILLET;
+    }
+
+    public void pay_paypal(){
+        this.estadoAtual = estadoAtual.PAYPAL;
+    }
+
+
+    // public void doPayment(PaymentType type) throws Exception {
+    //     switch (type) {
+    //         case CREDIT_CARD:
+    //             System.out.println("Do credit card payment!");
+    //             break;
+    //         case DEBIT:
+    //             System.out.println("Do debit payment!");
+    //             break;
+    //         case BILLET:
+    //             System.out.println("Do billet payment!");
+    //             break;
+    //         case PAYPAL:
+    //             System.out.println("Do paypal payment!");
+    //             break;
+    //         default:
+    //             throw new Exception("unknown payment method");
+    //     }
+    // }
 
 }
